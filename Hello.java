8ec62928280cdfad87231da326e7a1e923f05e97
Dilshad <dilshadkhan1202@gmail.com>
2020-07-18 15:12:59 -0700

    added all files

--- Hello.java
+++ Hello.java
@@ -4,6 +4,5 @@ class Hello{
 		System.out.println("Hello World");
 		System.out.println("Hello Dilshad");
 		System.out.println("This changes is for dev branch,master branch can't see this without merge branch");
-		System.out.println("Changes for GitHub dev branch");
 	}
 }

4718d3cd0d225ad4f75f412a88897e4f4a3f4e5e
Dilshad <dilshadkhan1202@gmail.com>
2020-07-18 13:05:44 -0700

    dev branch change 1

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ class Hello{
 		System.out.println("Hello Git");
 		System.out.println("Hello World");
 		System.out.println("Hello Dilshad");
+		System.out.println("This changes is for dev branch,master branch can't see this without merge branch");
 	}
 }
